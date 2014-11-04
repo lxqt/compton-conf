803f2661478a0b42621ea338d19db2b8b07d35e4
Ilya87 <yast4ik@yahoo.com>
2014-11-04 20:38:10 +0300

    Add Russian translation

---
+++ translations/compton-conf_ru.ts
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru" sourcelanguage="en_US">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../maindialog.ui" line="14"/>
+        <source>Special Effect Settings</source>
+        <translation>Настройки эффектов окон</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="24"/>
+        <source>Shadow</source>
+        <translation>Тень</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="30"/>
+        <source>The blur radius for shadows</source>
+        <translation>Радиус размытия для тени</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="40"/>
+        <source>The left offset for shadows</source>
+        <translation>Сдвиг влево для тени</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="54"/>
+        <source>The top offset for shadows</source>
+        <translation>Сдвиг вверх для тени</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="68"/>
+        <source>The translucency for shadows</source>
+        <translation>Полупрозрачность для тени</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="75"/>
+        <source>Color of shadow</source>
+        <translation>Цвет тени</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="89"/>
+        <source>Fix some weirdness with ARGB windows</source>
+        <translation>Исправить некоторые странности с окнами ARGB</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="92"/>
+        <source>Zero the part of the shadow&apos;s mask behind the window</source>
+        <translation>Обнулить часть маски тени за окном</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="99"/>
+        <source>Don&apos;t draw shadows on DND windows</source>
+        <translation>Не рисовать тень на окнах перетаскивания объектов</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="106"/>
+        <source>Avoid drawing shadows on dock/panel windows</source>
+        <translation>Избегать рисования теней на окнах доков/панелей</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="113"/>
+        <source>Enabled client-side shadows on windows</source>
+        <translation>Разрешить клиентские тени на окнах</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="120"/>
+        <source>Avoid drawing shadow on all shaped windows</source>
+        <translation>Избегать рисования теней на всех окнах, которые используют 
+расширение X Shape</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="138"/>
+        <source>Opacity</source>
+        <translation>Непрозрачность</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="147"/>
+        <source>The opacity for menus</source>
+        <translation>Непрозрачность меню</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="154"/>
+        <source>Default opacity of inactive windows</source>
+        <translation>Непрозрачность неактивных окон</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="161"/>
+        <source>Default opacity for active windows</source>
+        <translation>Непрозрачность активных окон</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="168"/>
+        <source>Opacity of window titlebars and borders</source>
+        <translation>Непрозрачность строки заголовка и границ</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="175"/>
+        <source>XRender backend: Step size for alpha pictures
+(Increasing it may result in less X resource usage)</source>
+        <translation>Сервер XRender: размер шага для прозрачных изображений
+(Увеличение его может привести к меньшему использованию
+ресурсов сервером X)</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="183"/>
+        <source>Dim inactive windows</source>
+        <translation>Затенять неактивные окна</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="190"/>
+        <source>Do not let dimness adjust based on window opacity</source>
+        <translation>Запретить непрозрачности окон влиять на затенение</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="197"/>
+        <source>Blur background of transparent windows</source>
+        <translation>Размывать фон прозрачного окна</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="204"/>
+        <source>Blur background of opaque windows with transparent frames as well</source>
+        <translation>Размывать фон непрозрачных окон с прозрачной рамкой</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="211"/>
+        <source>Do not let blur radius adjust based on window opacity</source>
+        <translation>Запретить непрозрачности окон влиять на радиус размытия</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="279"/>
+        <source>Fade</source>
+        <translation>Затухание</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="285"/>
+        <source>Fade windows during opacity changes</source>
+        <translation>Использовать затухание при изменении прозрачности окна</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="292"/>
+        <source>The time between steps in a fade in milliseconds</source>
+        <translation>Время между шагами изменения прозрачности (мс)</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="299"/>
+        <source>Opacity change between steps while fading in</source>
+        <translation>Шаг изменения прозрачности во время постепенного
+появления окна</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="306"/>
+        <source>Opacity change between steps while fading out</source>
+        <translation>Шаг изменения прозрачности во время постепенного
+исчезновения окна</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="313"/>
+        <source>Avoid fade windows in/out when opening/closing</source>
+        <translation>Не использовать постепенное появление (исчезновение) окна
+при открывании (закрывании)</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="356"/>
+        <source>Other</source>
+        <translation>Прочее</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="362"/>
+        <source>Not implemented yet</source>
+        <translation>Пока не реализовано</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.ui" line="375"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="179"/>
+        <source>About ComptonConf</source>
+        <translation>О ComptonConf</translation>
+    </message>
+    <message>
+        <location filename="../maindialog.cpp" line="180"/>
+        <source>ComptonConf - configuration tool for compton
+
+Copyright (C) 2013
+Author: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
+        <translation>ComptonConf - инструмент настройки compton
+
+Copyright (C) 2013
+Автор: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
+    </message>
+</context>
+</TS>
