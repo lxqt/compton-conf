1f6b91aca90f86dc132cfbd272838434b6164682
Jerome Leclanche <jerome@leclan.ch>
2015-10-31 00:09:13 +0200

    Fix mixed indents

--- maindialog.cpp
+++ maindialog.cpp
@@ -1,21 +1,21 @@
 /*
  * <one line to give the library's name and an idea of what it does.>
  * Copyright (C) 2013  <copyright holder> <email>
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  */
 
 #include "maindialog.h"
@@ -80,19 +80,19 @@ MainDialog::MainDialog(QString userConfigFile) {
     if(child->inherits("QCheckBox")) {
       int val = -1;
       if(config_lookup_bool(&config_, keyName.constData(), &val) == CONFIG_TRUE)
-	static_cast<QCheckBox*>(child)->setChecked((bool)val);
+        static_cast<QCheckBox*>(child)->setChecked((bool)val);
       connect(child, SIGNAL(toggled(bool)), SLOT(onButtonToggled(bool)));
     }
     else if(child->inherits("QDoubleSpinBox")) {
       double val;
       if(config_lookup_float(&config_, keyName.constData(), &val) == CONFIG_TRUE)
-	static_cast<QDoubleSpinBox*>(child)->setValue(val);
+        static_cast<QDoubleSpinBox*>(child)->setValue(val);
       connect(child, SIGNAL(valueChanged(double)), SLOT(onSpinValueChanged(double)));
     }
     else if(child->inherits("QSpinBox")) {
       int val;
       if(config_lookup_int(&config_, keyName.constData(), &val) == CONFIG_TRUE)
-	static_cast<QSpinBox*>(child)->setValue(val);
+        static_cast<QSpinBox*>(child)->setValue(val);
       connect(child, SIGNAL(valueChanged(int)), SLOT(onSpinValueChanged(int)));
     }
   }
@@ -134,7 +134,7 @@ void MainDialog::saveConfig() {
   qDebug() << userConfigFile_;
   // save the config file
   config_write_file(&config_, userConfigFile_.toLocal8Bit().constData());
-  
+
   // ask compton to reload the config
   QString displayName = qgetenv("DISPLAY");
   for(int i = 0; i < displayName.length(); ++i) {
