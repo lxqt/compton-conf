e813a25bb7ac4656e0895912202de5abef7c84ff
Dimitrios Glentadakis <dglent@free.fr>
2016-08-21 10:55:22 +0200

    Update Greek translation

---
+++ compton-conf_el.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../maindialog.ui" line="14"/>
+        <source>Special Effect Settings</source>
+        <translation>Ρυθμίσεις ειδικών τεχνασμάτων</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="24"/>
+        <source>Shadow</source>
+        <translation>Σκιά</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="30"/>
+        <source>The blur radius for shadows</source>
+        <translation>Η ακτίνα θόλωσης για της σκιάς</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="40"/>
+        <source>The left offset for shadows</source>
+        <translation>Η αριστερή μετατόπιση της σκιάς</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="54"/>
+        <source>The top offset for shadows</source>
+        <translation>Η άνω μετατόπιση της σκιάς</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="68"/>
+        <source>The translucency for shadows</source>
+        <translation>Η διαφάνεια της σκίασης</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="75"/>
+        <source>Color of shadow</source>
+        <translation>Χρώμα της σκίασης</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="89"/>
+        <source>Fix some weirdness with ARGB windows</source>
+        <translation>Διόρθωση μερικών παραξενιών με τα παράθυρα ARGB</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="92"/>
+        <source>Zero the part of the shadow&apos;s mask behind the window</source>
+        <translation>Απενεργοποίηση της σκίασης όπισθεν των παραθύρων</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="99"/>
+        <source>Don&apos;t draw shadows on DND windows</source>
+        <translation>Να μην αποτυπώνεται σκίαση σε παράθυρα DND</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="106"/>
+        <source>Avoid drawing shadows on dock/panel windows</source>
+        <translation>Καμιά σκίαση σε παράθυρα αποβάθρας/πίνακα</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="113"/>
+        <source>Enabled client-side shadows on windows</source>
+        <translation>Ενεργοποίηση σκίασης πλευρά πελάτη στα παράθυρα</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="120"/>
+        <source>Avoid drawing shadow on all shaped windows</source>
+        <translation>Καμιά σκίαση σε όλα τα μορφοποιημένα παράθυρα</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="138"/>
+        <source>Opacity</source>
+        <translation>Αδιαφάνεια</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="147"/>
+        <source>The opacity for menus</source>
+        <translation>Η αδιαφάνεια των μενού</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="154"/>
+        <source>Default opacity of inactive windows</source>
+        <translation>Αδιαφάνεια εξ ορισμού για τα με ενεργά παράθυρα</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="161"/>
+        <source>Default opacity for active windows</source>
+        <translation>Αδιαφάνεια εξ ορισμού για τα ενεργά παράθυρα</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="168"/>
+        <source>Opacity of window titlebars and borders</source>
+        <translation>Αδιαφάνεια για τα περιθώρια και τους τίτλους των παραθύρων</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="175"/>
+        <source>XRender backend: Step size for alpha pictures
+(Increasing it may result in less X resource usage)</source>
+        <translation>Σύστημα υποστήριξης XRender: Βηματικό μέγεθος εικόνων άλφα
+(αυξάνοντάς το μπορεί να οδηγήσει σε λιγότερη χρήση πόρων του X)</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="183"/>
+        <source>Dim inactive windows</source>
+        <translation>Υποσκίαση των απενεργοποιημένων παραθύρων</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="190"/>
+        <source>Do not let dimness adjust based on window opacity</source>
+        <translation>Να μην εξαρτάται η προσαρμογή της υποσκίασης στην αδιαφάνεια του παραθύρου</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="197"/>
+        <source>Blur background of transparent windows</source>
+        <translation>Θόλωμα του παρασκηνίου των διαφανών παραθύρων</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="204"/>
+        <source>Blur background of opaque windows with transparent frames as well</source>
+        <translation>Επίσης θόλωμα του παρασκηνίου των αδιαφανών παραθύρων με διάφανα πλαίσια</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="211"/>
+        <source>Do not let blur radius adjust based on window opacity</source>
+        <translation>Η προσαρμογή της ακτίνας θολώματος να μην γίνεται βάσει της αδιαφάνειας του παραθύρου</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="279"/>
+        <source>Fade</source>
+        <translation>Τέχνασμα εξασθένισης</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="285"/>
+        <source>Fade windows during opacity changes</source>
+        <translation>Εξασθένιση των παραθύρων κατά την αλλαγή της αδιαφάνειας </translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="292"/>
+        <source>The time between steps in a fade in milliseconds</source>
+        <translation>Το χρονικό διάστημα μεταξύ των βημάτων της εξασθένισης σε χιλιοστά του δευτερολέπτου</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="299"/>
+        <source>Opacity change between steps while fading in</source>
+        <translation>Αλλαγή της αδιαφάνειας σε κάθε βήμα κατά την βαθμιαία εμφάνιση</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="306"/>
+        <source>Opacity change between steps while fading out</source>
+        <translation>Αλλαγή της αδιαφάνειας σε κάθε βήμα κατά την βαθμιαία απόκρυψη</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="313"/>
+        <source>Avoid fade windows in/out when opening/closing</source>
+        <translation>Καμιά βαθμιαία εξασθένιση/απόκρυψη των παραθύρων κατά το άνοιγμα/κλείσιμο</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="356"/>
+        <source>Other</source>
+        <translation>Άλλα</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="362"/>
+        <source>Not implemented yet</source>
+        <translation>Δεν έχει υλοποιηθεί ακόμα</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="375"/>
+        <source>About</source>
+        <translation>Περί</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.cpp" line="179"/>
+        <source>About ComptonConf</source>
+        <translation>Περί του ComptonConf</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.cpp" line="180"/>
+        <source>ComptonConf - configuration tool for compton
+
+Copyright (C) 2013
+Author: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
+        <translation>ComptonConf - εργαλείο διαμόρφωσης του compton
+
+Πνευματικά δικαιώματα (C) 2013
+Συγγραφέας: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
+    </message>
+</context>
+</TS>
