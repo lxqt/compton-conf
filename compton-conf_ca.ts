e3172828ba611478388b08fcf33f8ede030e3c0c
Robert Antoni Buj Gelonch <robert.buj@gmail.com>
2017-07-14 13:12:55 +0200

    Updated Catalan translation

---
+++ compton-conf_ca.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../maindialog.ui" line="14"/>
+        <source>Special Effect Settings</source>
+        <translation>Ajusts dels efectes especials</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="24"/>
+        <source>Shadow</source>
+        <translation>Ombra</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="30"/>
+        <source>The blur radius for shadows</source>
+        <translation>El radi de difuminat per a les ombres</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="40"/>
+        <source>The left offset for shadows</source>
+        <translation>El desplaçament esquerre per a les ombres</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="54"/>
+        <source>The top offset for shadows</source>
+        <translation>El desplaçament superior per a les ombres</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="68"/>
+        <source>The translucency for shadows</source>
+        <translation>La translucència per a les ombres</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="75"/>
+        <source>Color of shadow</source>
+        <translation>Color de l&apos;ombra</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="89"/>
+        <source>Fix some weirdness with ARGB windows</source>
+        <translation>Corregeix algunes coses insòlites amb finestres ARGB</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="92"/>
+        <source>Zero the part of the shadow&apos;s mask behind the window</source>
+        <translation>Zero a la part de la màscara de l&apos;ombra del darrere de la finestra</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="99"/>
+        <source>Don&apos;t draw shadows on DND windows</source>
+        <translation>No dibuixis les ombres a les finestres DND</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="106"/>
+        <source>Avoid drawing shadows on dock/panel windows</source>
+        <translation>Evita dibuixar les ombres en les finestres de l&apos;acobladors o del plafó</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="113"/>
+        <source>Enable client-side shadows on windows</source>
+        <translation>Habilita les ombres del cantó del client en les finestres</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="120"/>
+        <source>Avoid drawing shadow on all shaped windows</source>
+        <translation>Evita dibuixar l&apos;ombra en totes les finestres amb formes</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="138"/>
+        <source>Opacity</source>
+        <translation>Opacitat</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="147"/>
+        <source>The opacity for menus</source>
+        <translation>L&apos;opacitat per als menús</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="154"/>
+        <source>Default opacity of inactive windows</source>
+        <translation>Opacitat predeterminada de les finestres inactives</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="161"/>
+        <source>Default opacity for active windows</source>
+        <translation>Opacitat predeterminada de les finestres actives</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="168"/>
+        <source>Opacity of window titlebars and borders</source>
+        <translation>Opacitat de les barres de títol i de les cantonades de la finestra</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="175"/>
+        <source>XRender backend: Step size for alpha pictures
+(Increasing it may result in less X resource usage)</source>
+        <translation>Dorsal XRender: Mida de la passa per a les imatges alfa
+(Augmentar-ho pot provocar un menor ús del recurs de les X)</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="183"/>
+        <source>Dim inactive windows</source>
+        <translation>Il·luminació de les finestres inactives</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="190"/>
+        <source>Do not let dimness adjust based on window opacity</source>
+        <translation>No permetis que la il·luminació s&apos;ajusti en funció de l&apos;opacitat de la finestra</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="197"/>
+        <source>Blur background of transparent windows</source>
+        <translation>Difumina el fons de les finestres transparents</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="204"/>
+        <source>Blur background of opaque windows with transparent frames as well</source>
+        <translation>Difumina també els fons de les finestres opaques amb marcs transparents</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="211"/>
+        <source>Do not let blur radius adjust based on window opacity</source>
+        <translation>No permetis que el radi de difuminat s&apos;ajusti en funció de l&apos;opacitat de la finestra</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="279"/>
+        <source>Fade</source>
+        <translation>Esvaïment</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="285"/>
+        <source>Fade windows during opacity changes</source>
+        <translation>Esvaeix les finestres durant els canvis d&apos;opacitat</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="292"/>
+        <source>The time between steps in a fade in milliseconds</source>
+        <translation>El temps entre les passes d&apos;un esvaïment en mil·lisegons</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="299"/>
+        <source>Opacity change between steps while fading in</source>
+        <translation>Canvi d&apos;opacitat entre les passes de l&apos;esvaïment d&apos;aparèixer</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="306"/>
+        <source>Opacity change between steps while fading out</source>
+        <translation>Canvi d&apos;opacitat entre les passes de l&apos;esvaïment de desaparèixer</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="313"/>
+        <source>Avoid fade windows in/out when opening/closing</source>
+        <translation>Evita l&apos;esvaïment d&apos;aparèixer o de desaparèixer de les finestres quan s&apos;obrin o es tanquin</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="356"/>
+        <source>Other</source>
+        <translation>Altres</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="362"/>
+        <source>Not implemented yet</source>
+        <translation>Encara no està implementat</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="375"/>
+        <source>About</source>
+        <translation>Quant a</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.cpp" line="179"/>
+        <source>About ComptonConf</source>
+        <translation>Quant a ComptonConf</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.cpp" line="180"/>
+        <source>ComptonConf - configuration tool for compton
+
+Copyright (C) 2013
+Author: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
+        <translation>ComptonConf - eina de configuració per a compton
+
+Drets d&apos;autor (C) 2013
+Autor: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
+    </message>
+</context>
+</TS>
