8ee747ccecbabdfb12770417b7a7d7e9e91e40ef
Toms Tilgalis <ttilgalis@gmail.com>
2017-09-05 23:24:31 +0300

    Latvian - Compton conf

---
+++ compton-conf_lv.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../maindialog.ui" line="14"/>
+        <source>Special Effect Settings</source>
+        <translation>Speciālo efektu iestatījumi</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="24"/>
+        <source>Shadow</source>
+        <translation>Ēna</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="30"/>
+        <source>The blur radius for shadows</source>
+        <translation>Ēnas izplūduma rādiuss</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="40"/>
+        <source>The left offset for shadows</source>
+        <translation>Ēnas nobīde no kreisās malas</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="54"/>
+        <source>The top offset for shadows</source>
+        <translation>Ēnas nobīde no augšējās malas</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="68"/>
+        <source>The translucency for shadows</source>
+        <translation>Ēnas caurspīdīgums</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="75"/>
+        <source>Color of shadow</source>
+        <translation>Ēnas krāsa</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="89"/>
+        <source>Fix some weirdness with ARGB windows</source>
+        <translation>Izlabot ARGB logu dīvainību</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="92"/>
+        <source>Zero the part of the shadow&apos;s mask behind the window</source>
+        <translation>Nodzēst maskas ēnas masku ārpus loga</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="99"/>
+        <source>Don&apos;t draw shadows on DND windows</source>
+        <translation>Nezīmēt ēnas uz DND logiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="106"/>
+        <source>Avoid drawing shadows on dock/panel windows</source>
+        <translation>Izvairīties no ēnu zīmēšanas uz paneļa logiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="113"/>
+        <source>Enable client-side shadows on windows</source>
+        <translation>Iespējot ēnas uz lietotāja puses logiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="120"/>
+        <source>Avoid drawing shadow on all shaped windows</source>
+        <translation>Izvairīties no ēnu zīmēšanas uz mērogotajiem logiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="138"/>
+        <source>Opacity</source>
+        <translation>Caurlaidība</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="147"/>
+        <source>The opacity for menus</source>
+        <translation>Izvēļņu caurlaidība</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="154"/>
+        <source>Default opacity of inactive windows</source>
+        <translation>Noklusējuma caurlaidība neaktīvajiem logiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="161"/>
+        <source>Default opacity for active windows</source>
+        <translation>Noklusējuma caurlaidība aktīvajiem logiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="168"/>
+        <source>Opacity of window titlebars and borders</source>
+        <translation>Logu galveņu un rāmju caurlaidība</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="175"/>
+        <source>XRender backend: Step size for alpha pictures
+(Increasing it may result in less X resource usage)</source>
+        <translation>XRender aizmugure: Soļa garums alfa attēliem
+(Palielināšana var novest pie mazākas X resursu izmantošanas)</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="183"/>
+        <source>Dim inactive windows</source>
+        <translation>Aptumšot nekatīvos logus</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="190"/>
+        <source>Do not let dimness adjust based on window opacity</source>
+        <translation>Neļaut aptumšojumam pielāgoties pamatojoties uz logu caurlaidību</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="197"/>
+        <source>Blur background of transparent windows</source>
+        <translation>Izpludināt fonu caurspīdīgajiem logiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="204"/>
+        <source>Blur background of opaque windows with transparent frames as well</source>
+        <translation>Izpludināt fonu arī necaurspīdīgajiem logiem ar caurspīdīgajiem rāmjiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="211"/>
+        <source>Do not let blur radius adjust based on window opacity</source>
+        <translation>Neļaut izplūduma rādiusa pielāgošanos atkarībā no loga caurlaidības</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="279"/>
+        <source>Fade</source>
+        <translation>Izgaisināt</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="285"/>
+        <source>Fade windows during opacity changes</source>
+        <translation>Izgaisināt logus caurlaidīguma maiņas laikā</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="292"/>
+        <source>The time between steps in a fade in milliseconds</source>
+        <translation>Laiks milisekundēs starp izgaisinājuma soļiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="299"/>
+        <source>Opacity change between steps while fading in</source>
+        <translation>Caurlaidīguma maiņa starp izgaisinājuma pazušanas soļiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="306"/>
+        <source>Opacity change between steps while fading out</source>
+        <translation>Caurlaidīguma maiņa starp izgaisinājuma soļiem</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="313"/>
+        <source>Avoid fade windows in/out when opening/closing</source>
+        <translation>Izvairīties no logu izgaisināšanas atverot un aizverot</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="356"/>
+        <source>Other</source>
+        <translation>Cits/-i</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="362"/>
+        <source>Not implemented yet</source>
+        <translation>Vēl nav īstenots</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="375"/>
+        <source>About</source>
+        <translation>Par</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.cpp" line="179"/>
+        <source>About ComptonConf</source>
+        <translation>Par ComptonConf</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.cpp" line="180"/>
+        <source>ComptonConf - configuration tool for compton
+
+Copyright (C) 2013
+Author: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
+        <translation>ComptonConf - compton konfigurācijas rīks
+
+Autortiesības (C) 2013
+Autors: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
+    </message>
+</context>
+</TS>
