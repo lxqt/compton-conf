05af3f835b91f24e4f3b987dcf38a515c0170b3f
Arif Budiman <arifpedia@gmail.com>
2017-09-12 13:57:54 +0700

    Create compton-conf_id.ts

---
+++ compton-conf_id.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="id">
+<context>
+    <name>MainDialog</name>
+    <message>
+        <location filename="../../maindialog.ui" line="14"/>
+        <source>Special Effect Settings</source>
+        <translation>Pengaturan Efek Khusus</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="24"/>
+        <source>Shadow</source>
+        <translation>Bayangan</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="30"/>
+        <source>The blur radius for shadows</source>
+        <translation>Radius blur untuk bayangan</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="40"/>
+        <source>The left offset for shadows</source>
+        <translation>Offset kiri untuk bayangan</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="54"/>
+        <source>The top offset for shadows</source>
+        <translation>Offset atas untuk bayangan</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="68"/>
+        <source>The translucency for shadows</source>
+        <translation>Keburaman untuk bayangan</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="75"/>
+        <source>Color of shadow</source>
+        <translation>Warna bayangan</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="89"/>
+        <source>Fix some weirdness with ARGB windows</source>
+        <translation>Perbaiki beberapa keanehan dengan jendela ARGB</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="92"/>
+        <source>Zero the part of the shadow&apos;s mask behind the window</source>
+        <translation>Setel topeng bayangan di balik jendela ke nol</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="99"/>
+        <source>Don&apos;t draw shadows on DND windows</source>
+        <translation>Jangan menggambar bayangan di jendela DND</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="106"/>
+        <source>Avoid drawing shadows on dock/panel windows</source>
+        <translation>Hindari menggambar bayangan di jendela dock/panel</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="113"/>
+        <source>Enable client-side shadows on windows</source>
+        <translation>Aktifkan bayangan sisi klien di jendela</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="120"/>
+        <source>Avoid drawing shadow on all shaped windows</source>
+        <translation>Hindari menggambar bayangan pada semua jendela berbentuk</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="138"/>
+        <source>Opacity</source>
+        <translation>Opasitas</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="147"/>
+        <source>The opacity for menus</source>
+        <translation>Opasitas untuk menu</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="154"/>
+        <source>Default opacity of inactive windows</source>
+        <translation>Opasitas standar jendela yang tidak aktif</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="161"/>
+        <source>Default opacity for active windows</source>
+        <translation>Opasitas standar jendela yang aktif</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="168"/>
+        <source>Opacity of window titlebars and borders</source>
+        <translation>Opasitas dari bilah judul dan border</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="175"/>
+        <source>XRender backend: Step size for alpha pictures
+(Increasing it may result in less X resource usage)</source>
+        <translation>XRender backend: Ukuran langkah untuk gambar alfa
+(Meningkatkannya bisa berakibat pada berkurangnya penggunaan sumber daya X)</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="183"/>
+        <source>Dim inactive windows</source>
+        <translation>Redupkan jendela yang tidak aktif</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="190"/>
+        <source>Do not let dimness adjust based on window opacity</source>
+        <translation>Tingkat keredupan tidak tergantung pada opasitas jendela</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="197"/>
+        <source>Blur background of transparent windows</source>
+        <translation>Latar belakang blur di jendela transparan</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="204"/>
+        <source>Blur background of opaque windows with transparent frames as well</source>
+        <translation>Latar belakang blur di jendela buram dengan bingkai transparan</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="211"/>
+        <source>Do not let blur radius adjust based on window opacity</source>
+        <translation>Radius blur tidak tergantung pada opasitas jendela</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="279"/>
+        <source>Fade</source>
+        <translation>Pudar</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="285"/>
+        <source>Fade windows during opacity changes</source>
+        <translation>Pudarkan jendela saat perubahan opasitas</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="292"/>
+        <source>The time between steps in a fade in milliseconds</source>
+        <translation>Waktu antara langkah memudar dalam milidetik</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="299"/>
+        <source>Opacity change between steps while fading in</source>
+        <translation>Perubahan opasitas antar langkah saat angsur jelas</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="306"/>
+        <source>Opacity change between steps while fading out</source>
+        <translation>Perubahan opasitas antar langkah saat angsur pudar</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="313"/>
+        <source>Avoid fade windows in/out when opening/closing</source>
+        <translation>Cegah angsur jelas/pudar saat membuka/menutup jendela</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="356"/>
+        <source>Other</source>
+        <translation>Lainnya</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="362"/>
+        <source>Not implemented yet</source>
+        <translation>Belum diimplementasikan</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="375"/>
+        <source>About</source>
+        <translation>Tantang</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.cpp" line="179"/>
+        <source>About ComptonConf</source>
+        <translation>Tantang ComptonConf</translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.cpp" line="180"/>
+        <source>ComptonConf - configuration tool for compton
+
+Copyright (C) 2013
+Author: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
+        <translation>ComptonConf - alat konfigurasi untuk compton
+
+Hak cipta (C) 2013
+Pencipta: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
+    </message>
+</context>
+</TS>
