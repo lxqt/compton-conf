005b6922b73b4480573fd97a37ec28492d8c3b5e
Arif Budiman <arifpedia@gmail.com>
2018-02-05 15:10:39 +0700

    Add pcmanfm-qt_id.ts and update compton-conf_id.ts (#327)
    * Added pavucontrol-qt/pavucontrol-qt_id.ts
    
    * Update obconf-qt/obconf-qt_id.ts
    
    * Update obconf-qt/obconf-qt_id.ts
    
    * Added pcmanfm-qt_id.ts

--- compton-conf_id.ts
+++ compton-conf_id.ts
@@ -71,7 +71,7 @@
     <message>
         <location filename="../../maindialog.ui" line="143"/>
         <source>Crop shadows of maximized windows from extended displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Crop bayangan jendela yang dimaksimalkan dari layar tambahan</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="151"/>
@@ -168,17 +168,17 @@
     <message>
         <location filename="../../maindialog.ui" line="381"/>
         <source>Rendering backend</source>
-        <translation type="unfinished"></translation>
+        <translation>Backend perenderan</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="393"/>
         <source>X Render</source>
-        <translation type="unfinished"></translation>
+        <translation>X Render</translation>
     </message>
     <message>
         <location filename="../../maindialog.ui" line="400"/>
         <source>GLX (OpenGL)</source>
-        <translation type="unfinished"></translation>
+        <translation>GLX (OpenGL)</translation>
     </message>
     <message>
         <source>Not implemented yet</source>
