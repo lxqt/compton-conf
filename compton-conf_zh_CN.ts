41a39d749c52e9fdd3c616c51db5d0033ef7b95a
Marcin Mikołajczak <me@m4sk.in>
2018-02-17 23:45:35 +0100

    Update translation files to current sources

--- compton-conf_zh_CN.ts
+++ compton-conf_zh_CN.ts
@@ -14,179 +14,184 @@
         <translation>阴影</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="30"/>
+        <location filename="../../maindialog.ui" line="67"/>
         <source>The blur radius for shadows</source>
         <translation>阴影模糊半径</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="40"/>
+        <location filename="../../maindialog.ui" line="77"/>
         <source>The left offset for shadows</source>
         <translation>阴影左部偏移</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="54"/>
+        <location filename="../../maindialog.ui" line="91"/>
         <source>The top offset for shadows</source>
         <translation>阴影顶部偏移</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="68"/>
+        <location filename="../../maindialog.ui" line="105"/>
         <source>The translucency for shadows</source>
         <translation>阴影透明度</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="75"/>
+        <location filename="../../maindialog.ui" line="122"/>
         <source>Color of shadow</source>
         <translation>阴影颜色</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="89"/>
+        <location filename="../../maindialog.ui" line="57"/>
         <source>Fix some weirdness with ARGB windows</source>
         <translation>修复 ARGB 窗口的怪诞表现</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="92"/>
+        <location filename="../../maindialog.ui" line="60"/>
         <source>Zero the part of the shadow&apos;s mask behind the window</source>
         <translation>归零窗口背后的阴影遮盖层</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="99"/>
+        <location filename="../../maindialog.ui" line="50"/>
         <source>Don&apos;t draw shadows on DND windows</source>
         <translation>不在 DND 窗口上绘制阴影</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="106"/>
+        <location filename="../../maindialog.ui" line="43"/>
         <source>Avoid drawing shadows on dock/panel windows</source>
         <translation>避免在 Dock 或面板窗口上绘制阴影</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="113"/>
+        <location filename="../../maindialog.ui" line="36"/>
         <source>Enable client-side shadows on windows</source>
         <translation>开启窗口阴影效果</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="120"/>
+        <location filename="../../maindialog.ui" line="136"/>
         <source>Avoid drawing shadow on all shaped windows</source>
         <translation>避免在所有已成形的窗口上绘制阴影</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="138"/>
+        <location filename="../../maindialog.ui" line="143"/>
+        <source>Crop shadows of maximized windows from extended displays</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../maindialog.ui" line="151"/>
         <source>Opacity</source>
         <translation>不透明度</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="147"/>
+        <location filename="../../maindialog.ui" line="160"/>
         <source>The opacity for menus</source>
         <translation>菜单不透明度</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="154"/>
+        <location filename="../../maindialog.ui" line="167"/>
         <source>Default opacity of inactive windows</source>
         <translation>不活动窗口默认透明度</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="161"/>
+        <location filename="../../maindialog.ui" line="174"/>
         <source>Default opacity for active windows</source>
         <translation>活动窗口默认透明度</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="168"/>
+        <location filename="../../maindialog.ui" line="181"/>
         <source>Opacity of window titlebars and borders</source>
         <translation>窗口标题栏与边框不透明度</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="175"/>
+        <location filename="../../maindialog.ui" line="188"/>
         <source>XRender backend: Step size for alpha pictures
 (Increasing it may result in less X resource usage)</source>
         <translation>XRender 后端: 透明图像的幅度值
 （增加此值可能导致 X 资源用量变少）</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="183"/>
+        <location filename="../../maindialog.ui" line="196"/>
         <source>Dim inactive windows</source>
         <translation>淡化不活动窗口</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="190"/>
+        <location filename="../../maindialog.ui" line="203"/>
         <source>Do not let dimness adjust based on window opacity</source>
         <translation>不使淡化度基于窗口不透明度调整</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="197"/>
+        <location filename="../../maindialog.ui" line="210"/>
         <source>Blur background of transparent windows</source>
         <translation>模糊化透明窗口背景</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="204"/>
+        <location filename="../../maindialog.ui" line="217"/>
         <source>Blur background of opaque windows with transparent frames as well</source>
         <translation>同时模糊化带透明框架的不透明窗口的背景</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="211"/>
+        <location filename="../../maindialog.ui" line="224"/>
         <source>Do not let blur radius adjust based on window opacity</source>
         <translation>不使模糊半径基于窗口不透明度调整</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="279"/>
+        <location filename="../../maindialog.ui" line="292"/>
         <source>Fade</source>
         <translation>淡入淡出</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="285"/>
+        <location filename="../../maindialog.ui" line="298"/>
         <source>Fade windows during opacity changes</source>
         <translation>在不透明度改变时淡入淡出窗口</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="292"/>
+        <location filename="../../maindialog.ui" line="305"/>
         <source>The time between steps in a fade in milliseconds</source>
         <translation>一次淡入淡出步骤间的时间（毫秒）</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="299"/>
+        <location filename="../../maindialog.ui" line="312"/>
         <source>Opacity change between steps while fading in</source>
         <translation>淡入步骤间改变不透明度</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="306"/>
+        <location filename="../../maindialog.ui" line="319"/>
         <source>Opacity change between steps while fading out</source>
         <translation>淡出步骤间改变不透明度</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="313"/>
+        <location filename="../../maindialog.ui" line="326"/>
         <source>Avoid fade windows in/out when opening/closing</source>
         <translation>避免在打开或关闭窗口时淡入淡出</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="356"/>
+        <location filename="../../maindialog.ui" line="369"/>
         <source>Other</source>
         <translation>其它</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="368"/>
+        <location filename="../../maindialog.ui" line="381"/>
         <source>Rendering backend</source>
         <translation>渲染后端</translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="380"/>
+        <location filename="../../maindialog.ui" line="393"/>
         <source>X Render</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="387"/>
+        <location filename="../../maindialog.ui" line="400"/>
         <source>GLX (OpenGL)</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../maindialog.ui" line="403"/>
+        <location filename="../../maindialog.ui" line="416"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../../maindialog.cpp" line="206"/>
+        <location filename="../../maindialog.cpp" line="209"/>
         <source>About ComptonConf</source>
         <translation>关于 ComptonConf</translation>
     </message>
     <message>
-        <location filename="../../maindialog.cpp" line="207"/>
+        <location filename="../../maindialog.cpp" line="210"/>
         <source>ComptonConf - configuration tool for compton
 
 Copyright (C) 2013
