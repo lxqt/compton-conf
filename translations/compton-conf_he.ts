72a921dc19ffecd1de75db275627f83db8b701cf
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-18 06:56:47 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (38 of 38 strings)
    
    Translation: LXQt/ComptonConf
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/compton-conf/he/

--- translations/compton-conf_he.ts
+++ translations/compton-conf_he.ts
@@ -6,168 +6,169 @@
     <message>
         <location filename="../maindialog.ui" line="14"/>
         <source>Special Effect Settings</source>
-        <translation></translation>
+        <translation>הגדרות אפקטים מיוחדים</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="24"/>
         <source>Shadow</source>
-        <translation></translation>
+        <translation>צל</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="67"/>
         <source>The blur radius for shadows</source>
-        <translation></translation>
+        <translation>רדיוס הטשטוש של הצלליות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="77"/>
         <source>The left offset for shadows</source>
-        <translation></translation>
+        <translation>ההיסט השמאלי לצלליות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="91"/>
         <source>The top offset for shadows</source>
-        <translation></translation>
+        <translation>ההיסט העליון לצלליות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="105"/>
         <source>The translucency for shadows</source>
-        <translation></translation>
+        <translation>שקיפות הצלליות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="122"/>
         <source>Color of shadow</source>
-        <translation></translation>
+        <translation>צבע הצללית</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="57"/>
         <source>Fix some weirdness with ARGB windows</source>
-        <translation></translation>
+        <translation>תיקון התנהגות מוזרה בחלונות ARGB</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="60"/>
         <source>Zero the part of the shadow&apos;s mask behind the window</source>
-        <translation></translation>
+        <translation>לאפס את החלק של מסיכת ההצללה מאחורי החלון</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="50"/>
         <source>Don&apos;t draw shadows on DND windows</source>
-        <translation></translation>
+        <translation>לא לצייר צלליות בחלונות במצב לא להפריע</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="43"/>
         <source>Avoid drawing shadows on dock/panel windows</source>
-        <translation></translation>
+        <translation>להימנע מציור צלליות על חלונות של מעגן/לוח</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="36"/>
         <source>Enable client-side shadows on windows</source>
-        <translation></translation>
+        <translation>הפעלת צלליות בצד הלקוח על חלונות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="136"/>
         <source>Avoid drawing shadow on all shaped windows</source>
-        <translation></translation>
+        <translation>להימנע מציור צללית על כל החלונות בעלי צורות שונות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="143"/>
         <source>Crop shadows of maximized windows from extended displays</source>
-        <translation></translation>
+        <translation>חיתוך צלליות של חלונות מוגדלים ממסכים חיצוניים</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="151"/>
         <source>Opacity</source>
-        <translation></translation>
+        <translation>אטימות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="160"/>
         <source>The opacity for menus</source>
-        <translation></translation>
+        <translation>אטימות לתפריטים</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="167"/>
         <source>Default opacity of inactive windows</source>
-        <translation></translation>
+        <translation>אטימות כבררת מחדל לחלונות בלתי פעילים</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="174"/>
         <source>Default opacity for active windows</source>
-        <translation></translation>
+        <translation>אטימות בררת מחדל לחלונות פעילים</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="181"/>
         <source>Opacity of window titlebars and borders</source>
-        <translation></translation>
+        <translation>אטימות של שורות כותרת וגבולות של חלונות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="188"/>
         <source>XRender backend: Step size for alpha pictures
 (Increasing it may result in less X resource usage)</source>
-        <translation></translation>
+        <translation>מנגנון XRender: גודל צעד לשקיפות תמונות
+(הגדלת הערך עשויה לצרוך פחות משאבי X)</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="196"/>
         <source>Dim inactive windows</source>
-        <translation></translation>
+        <translation>עמעום חלונות בלתי פעילים</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="203"/>
         <source>Do not let dimness adjust based on window opacity</source>
-        <translation></translation>
+        <translation>לא לאפשר לעמעום להיקבע על בסיס אטימות החלון</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="210"/>
         <source>Blur background of transparent windows</source>
-        <translation></translation>
+        <translation>טשטוש רקע של חלונות שקופים</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="217"/>
         <source>Blur background of opaque windows with transparent frames as well</source>
-        <translation></translation>
+        <translation>טשטוש גם רקע של חלונות אטומים עם מסגרות שקופות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="224"/>
         <source>Do not let blur radius adjust based on window opacity</source>
-        <translation></translation>
+        <translation>לא לאפשר לרדיוס הטשטוש להיקבע על בסיס אטימות חלון</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="292"/>
         <source>Fade</source>
-        <translation></translation>
+        <translation>דעיכה</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="298"/>
         <source>Fade windows during opacity changes</source>
-        <translation></translation>
+        <translation>דעיכת חלונות במהלך שינויי אטימות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="305"/>
         <source>The time between steps in a fade in milliseconds</source>
-        <translation></translation>
+        <translation>פרק הזמן שבין שלבים בדעיכה במילישניות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="312"/>
         <source>Opacity change between steps while fading in</source>
-        <translation></translation>
+        <translation>שינוי אטימות בין שלבים בעת התחדשות</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="319"/>
         <source>Opacity change between steps while fading out</source>
-        <translation></translation>
+        <translation>שינוי אטימות בין שלבים בעת דעיכה</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="326"/>
         <source>Avoid fade windows in/out when opening/closing</source>
-        <translation></translation>
+        <translation>להימנע מדעיכה/התחדשות בעת פתיחה/סגירה</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="369"/>
         <source>Other</source>
-        <translation></translation>
+        <translation>אחר</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="381"/>
         <source>Rendering backend</source>
-        <translation></translation>
+        <translation>מנגנון עיבוד</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="393"/>
@@ -182,12 +183,12 @@
     <message>
         <location filename="../maindialog.ui" line="416"/>
         <source>About</source>
-        <translation></translation>
+        <translation>על אודות</translation>
     </message>
     <message>
         <location filename="../maindialog.cpp" line="209"/>
         <source>About ComptonConf</source>
-        <translation></translation>
+        <translation>על אודות ComptonConf</translation>
     </message>
     <message>
         <location filename="../maindialog.cpp" line="210"/>
@@ -195,7 +196,10 @@
 
 Copyright (C) 2013
 Author: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
-        <translation></translation>
+        <translation>ComptonConf - כלי הגדרות ל־compton
+
+כל הזכויות שמורות (C) 2013
+יוצר: Hong Jen Yee (PCMan)‏ &lt;pcman.tw@gmail.com&gt;</translation>
     </message>
 </context>
 </TS>
