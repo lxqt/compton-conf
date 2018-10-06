5f14b55d40dc38ebaf63279f7f9739483bcbaa81
sp22 <sp22xss@gmail.com>
2018-10-06 12:48:54 +0000

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (38 of 38 strings)
    
    Translation: LXQt/ComptonConf
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/compton-conf/hi/

--- translations/compton-conf_hi.ts
+++ translations/compton-conf_hi.ts
@@ -6,173 +6,174 @@
     <message>
         <location filename="../maindialog.ui" line="14"/>
         <source>Special Effect Settings</source>
-        <translation></translation>
+        <translation>विशेष प्रभाव सेटिंग्स</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="24"/>
         <source>Shadow</source>
-        <translation></translation>
+        <translation>परछाई</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="67"/>
         <source>The blur radius for shadows</source>
-        <translation></translation>
+        <translation>परछाइयों का ब्लर रेडियस</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="77"/>
         <source>The left offset for shadows</source>
-        <translation></translation>
+        <translation>परछाइयों का बायां ओफ़्सेट</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="91"/>
         <source>The top offset for shadows</source>
-        <translation></translation>
+        <translation>परछाइयों का ऊपरी ओफ़्सेट</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="105"/>
         <source>The translucency for shadows</source>
-        <translation></translation>
+        <translation>परछाइयों की पारभासकता</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="122"/>
         <source>Color of shadow</source>
-        <translation></translation>
+        <translation>परछाइयों का रंग</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="57"/>
         <source>Fix some weirdness with ARGB windows</source>
-        <translation></translation>
+        <translation>ARGB खिड़कियों में विचित्रता ठीक करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="60"/>
         <source>Zero the part of the shadow&apos;s mask behind the window</source>
-        <translation></translation>
+        <translation>परछाई के मास्क के पीछे की परछाई को शुन्य करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="50"/>
         <source>Don&apos;t draw shadows on DND windows</source>
-        <translation></translation>
+        <translation>DND खिडकियों पे परछाइयों को ड्रा न करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="43"/>
         <source>Avoid drawing shadows on dock/panel windows</source>
-        <translation></translation>
+        <translation>Dock/panel खिड़कियों पर परछाइयों को ड्रा न करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="36"/>
         <source>Enable client-side shadows on windows</source>
-        <translation></translation>
+        <translation>क्लाइंट-साइड परछाइयों को सक्षम करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="136"/>
         <source>Avoid drawing shadow on all shaped windows</source>
-        <translation></translation>
+        <translation>आकारिक खिड़कियों पर परछाइयां न बनाएं</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="143"/>
         <source>Crop shadows of maximized windows from extended displays</source>
-        <translation></translation>
+        <translation>विस्तारित डिस्प्ले की अधिकतम आकार वाली खिड़कियों की परछाइयों को क्रॉप करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="151"/>
         <source>Opacity</source>
-        <translation></translation>
+        <translation>अपारदर्शता</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="160"/>
         <source>The opacity for menus</source>
-        <translation></translation>
+        <translation>मेनू की अपारदर्शता</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="167"/>
         <source>Default opacity of inactive windows</source>
-        <translation></translation>
+        <translation>निष्क्रिय खिड़की की डिफ़ॉल्ट अपारदर्शता</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="174"/>
         <source>Default opacity for active windows</source>
-        <translation></translation>
+        <translation>सक्रिय खिड़की की डिफ़ॉल्ट अपारदर्शता</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="181"/>
         <source>Opacity of window titlebars and borders</source>
-        <translation></translation>
+        <translation>शीर्षक बार एवं बार्डर की अपारदर्शता</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="188"/>
         <source>XRender backend: Step size for alpha pictures
 (Increasing it may result in less X resource usage)</source>
-        <translation></translation>
+        <translation>XRender बैकएंड: अल्फा पिक्चर की स्टेप साइज
+(इसे बढ़ने से X संसाधन उपयोग काम हो सकता है)</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="196"/>
         <source>Dim inactive windows</source>
-        <translation></translation>
+        <translation>निष्क्रिय खिड़कियों को मंद करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="203"/>
         <source>Do not let dimness adjust based on window opacity</source>
-        <translation></translation>
+        <translation>खिड़की की पारदर्शिता अनुसार मंदता को एडजस्ट न होने दें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="210"/>
         <source>Blur background of transparent windows</source>
-        <translation></translation>
+        <translation>पारदर्शी खिड़की के बैकग्राउंड को धुंदला करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="217"/>
         <source>Blur background of opaque windows with transparent frames as well</source>
-        <translation></translation>
+        <translation>पारदर्शी फ्रेम वाली अपारदर्शी खिड़की के बैकग्राउंड को भी धुंदला करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="224"/>
         <source>Do not let blur radius adjust based on window opacity</source>
-        <translation></translation>
+        <translation>खिड़की की अपारदर्शिता के अनुसार ब्लर रेडियस एडजस्ट न करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="292"/>
         <source>Fade</source>
-        <translation></translation>
+        <translation>फेड</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="298"/>
         <source>Fade windows during opacity changes</source>
-        <translation></translation>
+        <translation>अपारदर्शिता बदलाव के बीच खिड़कियों को फेड करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="305"/>
         <source>The time between steps in a fade in milliseconds</source>
-        <translation></translation>
+        <translation>फेड प्रक्रिया के स्टेप्स के बीच का समय (मिली सेकंड में )</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="312"/>
         <source>Opacity change between steps while fading in</source>
-        <translation></translation>
+        <translation>फेड होने के स्टेप्स के बीच अपारदर्शिता में बदलाव</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="319"/>
         <source>Opacity change between steps while fading out</source>
-        <translation></translation>
+        <translation>फेड आउट होते समय स्टेप्स के बीच अपारदर्शिता में बदलाव</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="326"/>
         <source>Avoid fade windows in/out when opening/closing</source>
-        <translation></translation>
+        <translation>खोलते/बंद करते समय खिड़की को फेड इन/आउट न करें</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="369"/>
         <source>Other</source>
-        <translation></translation>
+        <translation>अन्य</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="381"/>
         <source>Rendering backend</source>
-        <translation></translation>
+        <translation>प्रस्तुति बैकेंड</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="393"/>
         <source>X Render</source>
-        <translation></translation>
+        <translation>X रेंडर</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="400"/>
@@ -182,12 +183,12 @@
     <message>
         <location filename="../maindialog.ui" line="416"/>
         <source>About</source>
-        <translation></translation>
+        <translation>के बारे में</translation>
     </message>
     <message>
         <location filename="../maindialog.cpp" line="209"/>
         <source>About ComptonConf</source>
-        <translation></translation>
+        <translation>ComptonConf के बारे में</translation>
     </message>
     <message>
         <location filename="../maindialog.cpp" line="210"/>
@@ -195,7 +196,10 @@
 
 Copyright (C) 2013
 Author: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
-        <translation></translation>
+        <translation>ComptonConf - Compton के लिए विन्यास उपकरण
+
+कॉपीराइट (C) 2013
+रचनाकार: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     </message>
 </context>
 </TS>
