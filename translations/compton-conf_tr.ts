2a0fc07786f8c758269a2e6ec2acbb6eba5c91d3
Ayhan YALÇINSOY PhD. (Dr.) <ayhanyalcinsoy@pisilinux.org>
2018-12-06 13:28:31 +0000

    Translated using Weblate (Turkish)
    Currently translated at 84.2% (32 of 38 strings)
    
    Translation: LXQt/ComptonConf
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/compton-conf/tr/

--- translations/compton-conf_tr.ts
+++ translations/compton-conf_tr.ts
@@ -178,7 +178,7 @@
     <message>
         <location filename="../maindialog.ui" line="400"/>
         <source>GLX (OpenGL)</source>
-        <translation type="unfinished"></translation>
+        <translation>GLX (OpenGL)</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="416"/>
