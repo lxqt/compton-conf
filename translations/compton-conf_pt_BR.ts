d347040a2bbcba801be1dd35f8dea300ac399118
Davi Lopes <davilopes@sapo.pt>
2019-06-11 13:36:01 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (38 of 38 strings)
    
    Translation: LXQt/ComptonConf
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/compton-conf/pt_BR/

--- translations/compton-conf_pt_BR.ts
+++ translations/compton-conf_pt_BR.ts
@@ -138,7 +138,7 @@
     <message>
         <location filename="../maindialog.ui" line="298"/>
         <source>Fade windows during opacity changes</source>
-        <translation>Esmaecer janelas durante alterações de opacidade</translation>
+        <translation>Aplicar fade nas janelas em mudanças de opacidade</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="305"/>
