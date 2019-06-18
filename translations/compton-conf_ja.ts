7b87ff5a27b9efb469ac4ea61ea958a8aed73b7a
Ma. Ito <i_to_ma-lxqt014@yahoo.co.jp>
2019-06-18 11:28:00 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (38 of 38 strings)
    
    Translation: LXQt/ComptonConf
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/compton-conf/ja/

--- translations/compton-conf_ja.ts
+++ translations/compton-conf_ja.ts
@@ -41,12 +41,12 @@
     <message>
         <location filename="../maindialog.ui" line="57"/>
         <source>Fix some weirdness with ARGB windows</source>
-        <translation>ARGBウインドウの異常を直す</translation>
+        <translation>ARGBウィンドウの異常を直す</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="60"/>
         <source>Zero the part of the shadow&apos;s mask behind the window</source>
-        <translation>ウインドウ背後の影のマスクをゼロにする</translation>
+        <translation>ウィンドウ背後の影のマスクをゼロにする</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="50"/>
@@ -61,12 +61,12 @@
     <message>
         <location filename="../maindialog.ui" line="36"/>
         <source>Enable client-side shadows on windows</source>
-        <translation>ウインドウのクライアント側の影を有効にする</translation>
+        <translation>ウィンドウのクライアント側の影を有効にする</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="136"/>
         <source>Avoid drawing shadow on all shaped windows</source>
-        <translation>すべて整形済のウインドウに影を描画しない</translation>
+        <translation>すべて整形済のウィンドウに影を描画しない</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="143"/>
@@ -86,12 +86,12 @@
     <message>
         <location filename="../maindialog.ui" line="167"/>
         <source>Default opacity of inactive windows</source>
-        <translation>非アクティブウインドウの透明度</translation>
+        <translation>非アクティブウィンドウの透明度</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="174"/>
         <source>Default opacity for active windows</source>
-        <translation>アクティブウインドウの透明度</translation>
+        <translation>アクティブウィンドウの透明度</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="181"/>
@@ -107,27 +107,27 @@
     <message>
         <location filename="../maindialog.ui" line="196"/>
         <source>Dim inactive windows</source>
-        <translation>非アクティブウインドウを暗くする</translation>
+        <translation>非アクティブウィンドウを暗くする</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="203"/>
         <source>Do not let dimness adjust based on window opacity</source>
-        <translation>ウインドウの透明度にあわせた暗さの調節をしない</translation>
+        <translation>ウィンドウの透明度にあわせた暗さの調節をしない</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="210"/>
         <source>Blur background of transparent windows</source>
-        <translation>透明化ウインドウの背後をぼかす</translation>
+        <translation>透明化ウィンドウの背後をぼかす</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="217"/>
         <source>Blur background of opaque windows with transparent frames as well</source>
-        <translation>透明化フレームを伴う不透明ウインドウの背後をぼかす</translation>
+        <translation>透明化フレームを伴う不透明ウィンドウの背後をぼかす</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="224"/>
         <source>Do not let blur radius adjust based on window opacity</source>
-        <translation>ウインドウの透明度にあわせたぼかし半径の調節をしない</translation>
+        <translation>ウィンドウの透明度にあわせたぼかし半径の調節をしない</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="292"/>
@@ -137,7 +137,7 @@
     <message>
         <location filename="../maindialog.ui" line="298"/>
         <source>Fade windows during opacity changes</source>
-        <translation>透明度の変化中にウインドウをフェードする</translation>
+        <translation>透明度の変化中にウィンドウをフェードする</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="305"/>
@@ -157,7 +157,7 @@
     <message>
         <location filename="../maindialog.ui" line="326"/>
         <source>Avoid fade windows in/out when opening/closing</source>
-        <translation>ウインドウの開閉時にフェードしない</translation>
+        <translation>ウィンドウの開閉時にフェードしない</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="369"/>
@@ -177,12 +177,12 @@
     <message>
         <location filename="../maindialog.ui" line="400"/>
         <source>GLX (OpenGL)</source>
-        <translation>GLX (OpenGL)</translation>
+        <translation>GLX  (OpenGL)</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="416"/>
         <source>About</source>
-        <translation>情報</translation>
+        <translation>このアプリケーションについて</translation>
     </message>
     <message>
         <location filename="../maindialog.cpp" line="209"/>
