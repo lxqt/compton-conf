9c11ebc8bd58cd9ce55888a151ae98364266a303
Zmicer Turok <nashtlumach@gmail.com>
2019-09-11 11:32:50 +0000

    Translated using Weblate (Belarusian)
    Currently translated at 100.0% (38 of 38 strings)
    
    Translation: LXQt/ComptonConf
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/compton-conf/be/

--- translations/compton-conf_be.ts
+++ translations/compton-conf_be.ts
@@ -6,188 +6,189 @@
     <message>
         <location filename="../maindialog.ui" line="14"/>
         <source>Special Effect Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Налады эфектаў акон</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="24"/>
         <source>Shadow</source>
-        <translation type="unfinished"></translation>
+        <translation>Цень</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="67"/>
         <source>The blur radius for shadows</source>
-        <translation type="unfinished"></translation>
+        <translation>Радыус размыцця ценю</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="77"/>
         <source>The left offset for shadows</source>
-        <translation type="unfinished"></translation>
+        <translation>Зрух ценю ўлева</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="91"/>
         <source>The top offset for shadows</source>
-        <translation type="unfinished"></translation>
+        <translation>Зрух ценю ўверх</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="105"/>
         <source>The translucency for shadows</source>
-        <translation type="unfinished"></translation>
+        <translation>Паўпразрыстасць ценю</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="122"/>
         <source>Color of shadow</source>
-        <translation type="unfinished"></translation>
+        <translation>Колер ценю</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="57"/>
         <source>Fix some weirdness with ARGB windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Выправіць пэўныя супярэчнасці акон ARGB</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="60"/>
         <source>Zero the part of the shadow&apos;s mask behind the window</source>
-        <translation type="unfinished"></translation>
+        <translation>Абнуліць частку маскі ценю за акном</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="50"/>
         <source>Don&apos;t draw shadows on DND windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Не адмалёўваць цень на вокнах перацягвання</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="43"/>
         <source>Avoid drawing shadows on dock/panel windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Пазбягаць адмалёўкі ценяў на вокнах докаў/панэляў</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="36"/>
         <source>Enable client-side shadows on windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Уключыць цені на вокнах на баку кліента</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="136"/>
         <source>Avoid drawing shadow on all shaped windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Пазбягаць адмалёўкі ценяў на ўсіх вокнах, што выкарыстоўваюць X Shape</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="143"/>
         <source>Crop shadows of maximized windows from extended displays</source>
-        <translation type="unfinished"></translation>
+        <translation>Абразаць цені разгорнутых акон з іншых дысплеяў</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="151"/>
         <source>Opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Непразрыстасць</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="160"/>
         <source>The opacity for menus</source>
-        <translation type="unfinished"></translation>
+        <translation>Непразрыстасць меню</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="167"/>
         <source>Default opacity of inactive windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Прадвызначаная непразрыстасць неактыўных акон</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="174"/>
         <source>Default opacity for active windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Прадвызначаная непразрыстасць актыўных акон</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="181"/>
         <source>Opacity of window titlebars and borders</source>
-        <translation type="unfinished"></translation>
+        <translation>Непразрыстасць загалоўка і межаў</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="188"/>
         <source>XRender backend: Step size for alpha pictures
 (Increasing it may result in less X resource usage)</source>
-        <translation type="unfinished"></translation>
+        <translation>Рухавік XRender: памер кроку для празрыстых выяў
+(Павелічэнне можа паменшыць выкарыстанне рэсурсаў X-серверам)</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="196"/>
         <source>Dim inactive windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Зацяняць неактыўныя вокны</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="203"/>
         <source>Do not let dimness adjust based on window opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Забараніць непразрыстасці ўплываць на зацяненне</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="210"/>
         <source>Blur background of transparent windows</source>
-        <translation type="unfinished"></translation>
+        <translation>Размываць фон празрыстага акна</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="217"/>
         <source>Blur background of opaque windows with transparent frames as well</source>
-        <translation type="unfinished"></translation>
+        <translation>Размываць фон непразрыстых акон з празрыстаю рамкаю</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="224"/>
         <source>Do not let blur radius adjust based on window opacity</source>
-        <translation type="unfinished"></translation>
+        <translation>Забараніць непразрыстасці акон уплываць на радыус размыцця</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="292"/>
         <source>Fade</source>
-        <translation type="unfinished"></translation>
+        <translation>Згасанне</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="298"/>
         <source>Fade windows during opacity changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Выкарыстоўваць згасанне пры змене празрыстасці акна</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="305"/>
         <source>The time between steps in a fade in milliseconds</source>
-        <translation type="unfinished"></translation>
+        <translation>Прамежак часу паміж крокамі змены празрыстасці (мс)</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="312"/>
         <source>Opacity change between steps while fading in</source>
-        <translation type="unfinished"></translation>
+        <translation>Крок змены празрыстасці падчас паступовага з’яўлення акна</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="319"/>
         <source>Opacity change between steps while fading out</source>
-        <translation type="unfinished"></translation>
+        <translation>Крок змены празрыстасці падчас паступовага знікання акна</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="326"/>
         <source>Avoid fade windows in/out when opening/closing</source>
-        <translation type="unfinished"></translation>
+        <translation>Не выкарыстоўваць паступовае з’яўленне (зніканне) акна падчас адкрыцця (закрыцця)</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="369"/>
         <source>Other</source>
-        <translation type="unfinished"></translation>
+        <translation>Іншае</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="381"/>
         <source>Rendering backend</source>
-        <translation type="unfinished"></translation>
+        <translation>Рухавік адмалёўкі</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="393"/>
         <source>X Render</source>
-        <translation type="unfinished"></translation>
+        <translation>X Render</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="400"/>
         <source>GLX (OpenGL)</source>
-        <translation type="unfinished"></translation>
+        <translation>GLX (OpenGL)</translation>
     </message>
     <message>
         <location filename="../maindialog.ui" line="416"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Пра праграму</translation>
     </message>
     <message>
         <location filename="../maindialog.cpp" line="209"/>
         <source>About ComptonConf</source>
-        <translation type="unfinished"></translation>
+        <translation>Пра ComptonConf</translation>
     </message>
     <message>
         <location filename="../maindialog.cpp" line="210"/>
@@ -195,7 +196,10 @@
 
 Copyright (C) 2013
 Author: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>ComptonConf - інструмент для наладкі Compton
+
+Copyright (C) 2013
+Author: Hong Jen Yee (PCMan) &lt;pcman.tw@gmail.com&gt;</translation>
     </message>
 </context>
 </TS>
